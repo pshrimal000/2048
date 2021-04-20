f0c0faa35086cc2b02e478d87c3799229c286e08
pshrimal000 <pshrimal000@gmail.com>
2021-04-20 11:41:53 +0530

    Updates

---
+++ precache-manifest.d3a4e70b82acf24f7f5a16b8fc959e9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fb573cb96148a13f921",
+    "url": "/2048/static/css/main.bccd701c.chunk.css"
+  },
+  {
+    "revision": "1fb573cb96148a13f921",
+    "url": "/2048/static/js/main.094757b4.chunk.js"
+  },
+  {
+    "revision": "e387bff5e0b18c0491fd",
+    "url": "/2048/static/js/runtime~main.f72a3c6a.js"
+  },
+  {
+    "revision": "6aa257a436956bac3d77",
+    "url": "/2048/static/js/2.19041111.chunk.js"
+  },
+  {
+    "revision": "6039d3c079cedec5ae65c1d2d5aee8c5",
+    "url": "/2048/index.html"
+  }
+];
